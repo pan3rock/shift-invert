710f50baea872e9f2b4c73d93e64a0c9e3c75914
Lei Pan <panlei7@outlook.com>
2020-06-11 07:50:48 +0800

    [add] performance test

--- test/test_solver.cc
+++ test/test_solver.cc
@@ -1,5 +1,6 @@
 #include "catch.hpp"
 #include "shiftinvert_solver.hpp"
+#include "timer.hpp"
 
 #include <Eigen/Dense>
 #include <complex>
@@ -33,4 +34,27 @@ TEST_CASE("run through", "[solver]") {
   ev2 = sis.compute(sigma);
   fmt::print("eigenvalues near to ({:12.5f}, {:12.5f}): ({:12.5f}, {:12.5f})\n",
              sigma.real(), sigma.imag(), ev2.real(), ev2.imag());
+}
+
+TEST_CASE("performance", "[solver]") {
+  int ndim = 20;
+  MatrixXd matA = MatrixXd::Random(ndim, ndim);
+  MatrixXd matB = MatrixXd::Random(ndim, ndim);
+  Timer::begin("GeneralizedEigenSolver");
+  GeneralizedEigenSolver<MatrixXd> ges(matA, matB);
+  VectorXcd ev = ges.eigenvalues();
+  Timer::end("GeneralizedEigenSolver");
+  for (int i = 0; i < ev.size(); ++i) {
+    fmt::print("({:12.5f}, {:12.5f})\n", ev(i).real(), ev(i).imag());
+  }
+  Timer::begin("shift-invert");
+  int maxiter = 20;
+  double tol = 1.0e-5;
+  ShiftinvertSolver sis(matA, matB, maxiter, tol);
+  std::complex<double> sigma = 0.59;
+  std::complex<double> ev2 = sis.compute(sigma);
+  Timer::end("shift-invert");
+  fmt::print("eigenvalues near to ({:12.5f}, {:12.5f}): ({:12.5f}, {:12.5f})\n",
+             sigma.real(), sigma.imag(), ev2.real(), ev2.imag());
+  std::cout << Timer::summery() << std::endl;
 }
